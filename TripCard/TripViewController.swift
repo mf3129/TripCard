ac5605964ee7aea3f3d3860c046ad2a565d847a1
Makan Fofana <mfofanaT99@gmail.com>
2019-05-17 23:24:18 -0400

    Initial Commit

---
+++ TripCard/TripViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TripViewController.swift
+//  TripCard
+//
+//  Makan Fofana
+//
+
+import UIKit
+
+class TripViewController: UIViewController {
+
+    @IBOutlet var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Apply blurring effect
+        backgroundImageView.image = UIImage(named: "cloud")
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = view.bounds
+        backgroundImageView.addSubview(blurEffectView)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
