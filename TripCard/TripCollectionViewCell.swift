35b9836906601ebe7bc6ea9cc5ec39bfb2381448
Makan Fofana <mfofanaT99@gmail.com>
2019-05-24 09:43:41 -0400

    Completion of the UI constraints for the view

---
+++ TripCard/TripCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TripCollectionViewCell.swift
+//  TripCard
+//
+//  Created by Makan fofana on 5/24/19.
+//  Copyright © 2019 MakanFofana. All rights reserved.
+//
+
+import UIKit
+
+class TripCollectionViewCell: UICollectionViewCell {
+    
+}
