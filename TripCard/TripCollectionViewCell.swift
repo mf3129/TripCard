987b31b1279e3e7ad66b90035c5c0a15b7b9b1f0
Makan Fofana <mfofanaT99@gmail.com>
2019-05-24 09:56:07 -0400

    creation of the UI cell swift file

--- TripCard/TripCollectionViewCell.swift
+++ TripCard/TripCollectionViewCell.swift
@@ -10,4 +10,20 @@ import UIKit
 
 class TripCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var countryLabel: UILabel!
+    @IBOutlet var totalDaysLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    
+    var isLiked: Bool = false {
+        didSet {
+            if isLiked {
+                likeButton.setImage(UIImage(named: "heartfull"), for: .normal)
+            } else {
+                likeButton.setImage(UIImage(named: "heart"), for: .normal)
+            }
+        }
+    }
 }
