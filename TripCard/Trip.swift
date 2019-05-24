4e4252db0039d6bdf11e1b358ec55be1ab99f797
Makan Fofana <mfofanaT99@gmail.com>
2019-05-24 10:07:18 -0400

    creation of data model for trip information.

---
+++ TripCard/Trip.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  TripCard
+//
+//  Created by Makan fofana on 5/24/19.
+//  Copyright © 2019 MakanFofana. All rights reserved.
+//
+
+import Foundation
+
+
+struct Trip {
+    var tripId = ""
+    var city = ""
+    var country = ""
+    var featuredImage: UIImage?
+    var price: Int = 0
+    var totalDays: Int = 0
+    var isLiked: Bool = false
+    
+    
+}
